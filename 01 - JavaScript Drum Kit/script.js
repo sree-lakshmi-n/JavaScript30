4e957473010fc6d0b918e6b87c806e0088c4797d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-16 16:23:39 +0530

    Adds 'playing' class for the visual effects and removes it after 100ms

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -3,8 +3,13 @@
 // Finding if a key is clicked
 // If clicked, returns the data-key value
 const keys = document.getElementsByClassName("key");
+const audio = document.getElementsByTagName("audio");
 Array.from(keys).forEach((element) => {
   element.addEventListener("click", () => {
-    console.log(element.getAttribute("data-key"));
+    dataKey = element.getAttribute("data-key");
+    // Adds 'playing' class for the visual effects
+    element.classList.add("playing");
+    // Removes 'playing' class after 100ms
+    setTimeout(() => element.classList.remove("playing"), 100);
   });
 });
