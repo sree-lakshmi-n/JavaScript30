7b778e8335b57bb47f1d1c3e3f8c25faaec368ad
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-16 17:26:29 +0530

    Checking for the audio element with the clicked key keyCode. If found, log the keyCode.

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -4,13 +4,22 @@
 // If clicked, returns the data-key value
 const keys = document.getElementsByClassName("key");
 const audio = document.getElementsByTagName("audio");
+
 Array.from(keys).forEach((element) => {
   element.addEventListener("click", () => {
     const keyCode = element.getAttribute("data-key");
-
+    checkKeyCode(keyCode);
     // Adds 'playing' class for the visual effects
     element.classList.add("playing");
     // Removes 'playing' class after 100ms
     setTimeout(() => element.classList.remove("playing"), 100);
   });
 });
+// Checking for the audio element with the clicked key keyCode
+// If found, log the keyCode
+const checkKeyCode = (keyCode) => {
+  Array.from(audio).forEach((element) => {
+    const audioKey = element.getAttribute("data-key");
+    if (keyCode === audioKey) console.log(audioKey);
+  });
+};
