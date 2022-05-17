09271e400b480ff08bda20cfe4d5ef99c7f292e4
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-17 10:56:41 +0530

    Playing drums by clicking

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,27 +1,35 @@
 `use strict`;
 
+// Playing by clicking
 // Finding if a key is clicked
 // If clicked, returns the data-key value
 const keys = document.getElementsByClassName("key");
-const audio = document.getElementsByTagName("audio");
 
 Array.from(keys).forEach((element) => {
   element.addEventListener("click", () => {
-    const keyCode = element.getAttribute("data-key");
-    checkKeyCode(keyCode);
+    const key = element.getAttribute("data-key");
+    const audio = document.querySelector(
+      `audio[data-key="${key.toUpperCase()}"]`
+    );
+    audio.play();
     // Adds 'playing' class for the visual effects
     element.classList.add("playing");
     // Removes 'playing' class after 100ms
     setTimeout(() => element.classList.remove("playing"), 100);
   });
 });
-// Checking for the audio element with the clicked key keyCode
-// If found, play the sound
-const checkKeyCode = (keyCode) => {
-  Array.from(audio).forEach((element) => {
-    const audioKey = element.getAttribute("data-key");
-    if (keyCode === audioKey) {
-      element.play();
-    }
-  });
-};
+
+// Playing using keyboard
+// Checks if a key is pressed
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(
+    `audio[data-key="${e.key.toUpperCase()}"]`
+  );
+  const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
+  if (audio === null) return;
+  key.classList.add("playing");
+  setTimeout(() => {
+    key.classList.remove("playing");
+  }, 100);
+  audio.play();
+});
