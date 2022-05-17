7b6fa0096df9ca7f7a131d17f6140754d15bfe03
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-17 15:06:31 +0530

    Made the code DRY. Added Comments

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -12,18 +12,12 @@ Array.from(keys).forEach((element) => {
     const audio = document.querySelector(
       `audio[data-key="${key.toUpperCase()}"]`
     );
-    // Play the corresponding audio
-    audio.play();
-    // Adds 'playing' class for the visual effects
-    element.classList.add("playing");
-    // Removes 'playing' class after 100ms
-    setTimeout(() => element.classList.remove("playing"), 100);
+    transitionAndPlay(audio, element);
   });
 });
 
 // Playing using keyboard
-// Checks if a key is pressed
-window.addEventListener("keydown", (e) => {
+const startDrumKeyBoard = (e) => {
   // Fetches the corresponding audio element
   const audio = document.querySelector(
     `audio[data-key="${e.key.toUpperCase()}"]`
@@ -32,12 +26,21 @@ window.addEventListener("keydown", (e) => {
   const key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
   // If the key pressed isn't valid, return
   if (audio === null) return;
-  // Added visual effects
+  transitionAndPlay(audio, key);
+};
+
+// Checks if a key is pressed
+window.addEventListener("keydown", startDrumKeyBoard);
+
+// Plays the audio and adds visual effects for 100ms
+const transitionAndPlay = (audio, key) => {
+  audio.currentTime = 0; // rewind the audio to the start
+  // Play the corresponding audio
+  audio.play();
+  // Adds 'playing' class for the visual effects
   key.classList.add("playing");
-  // Visual effects to be removed after 100ms
+  // Removes 'playing' class after 100ms
   setTimeout(() => {
     key.classList.remove("playing");
   }, 100);
-  // Play the corresponding audio
-  audio.play();
-});
+};
