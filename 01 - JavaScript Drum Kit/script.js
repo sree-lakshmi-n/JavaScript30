4a7af8aadef5b2e37605014b7381f2d28e91af1c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-05-17 16:08:59 +0530

    Used transitionend event listener instead of setTimeout() to remove visual effects from keys

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,19 +1,31 @@
 `use strict`;
 
-// Playing by clicking
 // Fetches all the key elements
 const keys = document.getElementsByClassName("key");
-// Finding if a key is clicked
+
+// Removes the visual effects after the transition ends
+// Instead of setTimeout()
+const removeTransition = (e) => {
+  e.target.classList.remove("playing");
+};
+
+// Fetches audio according to the clicked data-key
+// Calls transitionAndPlay()
+const startDrumOnClick = (element) => {
+  //   console.log(element.target);
+  // Gets the data-key value of the element
+  const key = element.getAttribute("data-key");
+  // Fetches the corresponding audio element
+  const audio = document.querySelector(
+    `audio[data-key="${key.toUpperCase()}"]`
+  );
+  transitionAndPlay(audio, element);
+};
+
+// Finds if a key is clicked or if it's transition has ended
 Array.from(keys).forEach((element) => {
-  element.addEventListener("click", () => {
-    // Gets the data-key value of the element
-    const key = element.getAttribute("data-key");
-    // Fetches the corresponding audio element
-    const audio = document.querySelector(
-      `audio[data-key="${key.toUpperCase()}"]`
-    );
-    transitionAndPlay(audio, element);
-  });
+  element.addEventListener("transitionend", removeTransition);
+  element.addEventListener("click", () => startDrumOnClick(element));
 });
 
 // Playing using keyboard
@@ -40,7 +52,7 @@ const transitionAndPlay = (audio, key) => {
   // Adds 'playing' class for the visual effects
   key.classList.add("playing");
   // Removes 'playing' class after 100ms
-  setTimeout(() => {
-    key.classList.remove("playing");
-  }, 100);
+  //   setTimeout(() => {
+  //     key.classList.remove("playing");
+  //   }, 100);
 };
